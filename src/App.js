acbc50897cf8a45032a98192b8d35abaee81b266
mehmetozk4n6 <81770287+mehmetozk4n6@users.noreply.github.com>
2022-04-06 05:22:16 +0300

    Context kullanılarak oluşturuldu.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "./components/Container";
+import { CityProvider } from "./context/CityContext";
+
+function App() {
+  return (
+    <CityProvider>
+      <div className="App">
+        <Container />
+      </div>
+    </CityProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Container.js
@@ -0,0 +1,12 @@
+import Body from "./Body";
+import Header from "./Header";
+function Container() {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default Container;
